e11b7b678eafa8dc07f7b002554980ea5e394c8e
jitesh kumar <jitesh.kumar05official@gmail.com>
2025-10-26 07:02:41 +0530

    csmacd

--- csmaca.c
+++ csmaca.c
@@ -3,59 +3,41 @@
 #include <pthread.h>
 #include <stdatomic.h>
 #include <unistd.h>
-
-atomic_int x = 0;
-
-static inline int get(void) {
-    return atomic_load(&x);
+#define NUM_STATIONS 3
+#define NUM_ATTEMPTS 5
+pthread_mutex_t channel;
+void* station(void *arg){
+int id=*(int*) arg;
+srand(time(NULL)+id);
+int i;
+for(i=0;i<NUM_ATTEMPTS;i++){
+printf("Station%d is trying to access the channel \n",id);
+if(pthread_mutex_trylock(&channel)==0){
+printf("Station %d got access to the channel\n",id);
+sleep(1);
+printf("Station %d released the channel\n",id);
+pthread_mutex_unlock(&channel);
 }
-
-static inline void put(void) {
-    atomic_fetch_add(&x, 1);
+else{
+printf("station % d found channel busy, retrying\n",id);
 }
-
-void* node(void* arg) {
-    int name = atoi((const char*)arg);
-    int seq1, seq2, i = 0;
-
-    while (i < 3) {
-        seq1 = get();
-        seq2 = get();
-
-        if (seq1 == seq2) {
-            put();
-            seq1 = get();
-            printf("Station %d transmitting frame %d\n", name, ++i);
-            sleep(2);
-            seq2 = get();
-
-            if (seq1 != seq2) {
-                printf("Station %d collision occurred on frame %d\n", name, i);
-                --i;
-                sleep(2);
-            } else {
-                printf("Station %d completed frame %d\n", name, i);
-            }
-        }
-
-        sleep(1);
-    }
-
-    printf("Station %d finished sending 3 frames\n", name);
-    return NULL;
+sleep(rand()%3+1);
 }
-
-int main(void) {
-    pthread_t t1, t2, t3;
-
-    pthread_create(&t1, NULL, node, "1");
-    pthread_create(&t2, NULL, node, "2");
-    pthread_create(&t3, NULL, node, "3");
-
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
-    pthread_join(t3, NULL);
-
-    printf("All stations finished\n");
-    return 0;
+pthread_exit(NULL);
+}
+int main(){
+pthread_t threads[NUM_STATIONS];
+int ids[NUM_STATIONS];
+int i;
+pthread_mutex_init(&channel,NULL);
+for(i=0;i<NUM_STATIONS;i++){
+ids[i]=i+1;
+pthread_create(&threads[i],NULL,station,(void*)&ids[i]);
+}
+for(i=0;i<NUM_STATIONS;i++){
+pthread_join(threads[i],NULL);
+}
+pthread_mutex_destroy(&channel);
+printf("Simulation finished\n");
+return 0;
 }
